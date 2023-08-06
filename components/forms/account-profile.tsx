acf975f34379ca9178a6cb5732850034df67310b
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-06 13:24:27 +0100

    added types for form + hooks

--- components/forms/account-profile.tsx
+++ components/forms/account-profile.tsx
@@ -1,7 +1,18 @@
 "use client";
 
-import { Form } from "@/components/ui/form";
+import * as z from "zod";
 import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { userSchema } from "@/lib/validations/user";
 
@@ -18,7 +29,7 @@ interface AccountProfileProps {
 }
 
 const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
-  const form = useForm({
+  const form = useForm<z.infer<typeof userSchema>>({
     resolver: zodResolver(userSchema),
     defaultValues: {
       profile_photo: "",
@@ -27,7 +38,34 @@ const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
       bio: "",
     },
   });
-  return <div>Account Profile</div>;
+
+  function onSubmit(values: z.infer<typeof userSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default AccountProfile;
