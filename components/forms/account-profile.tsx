32db4ae4c159ba2f243ceb09b1c10f23118ec8e3
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-06 13:01:10 +0100

    acc profile + install shadcn

---
+++ components/forms/account-profile.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface AccountProfileProps {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
+const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
+  return <div>Account Profile</div>;
+};
+
+export default AccountProfile;
