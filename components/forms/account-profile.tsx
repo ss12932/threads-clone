9eaa55063d2b89c6ea742cd383448c1cd8d5583b
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-06 13:13:18 +0100

    acc profile started + zod + userschema initial

--- components/forms/account-profile.tsx
+++ components/forms/account-profile.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userSchema } from "@/lib/validations/user";
+
 interface AccountProfileProps {
   user: {
     id: string;
@@ -13,6 +18,15 @@ interface AccountProfileProps {
 }
 
 const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
+  const form = useForm({
+    resolver: zodResolver(userSchema),
+    defaultValues: {
+      profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
+  });
   return <div>Account Profile</div>;
 };
 
