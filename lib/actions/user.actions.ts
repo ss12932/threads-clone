c485314ec9f2c8ea0340d687d2b6dace8a581707
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-08 23:52:38 +0100

    fixed nextjs error with community imgs

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,9 +3,9 @@
 import { FilterQuery, SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
 
-import Community from "../models/community.model";
-import Thread from "../models/thread.model";
-import User from "../models/user.model";
+import Community from "@/lib/models/community.model";
+import Thread from "@/lib/models/thread.model";
+import User from "@/lib/models/user.model";
 
 import { connectToDB } from "../mongoose";
 
