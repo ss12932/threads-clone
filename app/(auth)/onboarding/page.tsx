d77c5d5f6749d0abdb7227dc633ea0012dba5bb3
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-09 11:18:45 +0100

    fix onboarding errors

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,16 @@
 import AccountProfile from "@/components/forms/account-profile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 async function Page() {
   const user = await currentUser();
 
-  const userInfo = {};
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
     id: user?.id,
